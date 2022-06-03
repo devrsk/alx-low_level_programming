94eabd35e1f16ef6a84b2270bf218cd79c11a55c
Raheem Salifu Kasim <r.kasim@aol.com>
2022-06-03 22:32:52 +0000

    Update

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,10 +7,11 @@
  * Description: get a random number, print it
  * and state if it is positive, negative or zero
  * Return: 0
- */
+**/
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
