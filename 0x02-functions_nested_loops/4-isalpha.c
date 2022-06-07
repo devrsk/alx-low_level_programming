11f2f02dea3f6e3a496eadcbcfd8b5ac1daa0ec1
Raheem Salifu Kasim <r.kasim@aol.com>
2022-06-07 22:37:38 +0000

    Write a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *_isalpha - checks if char is alphabet or ot
+ *_isalpha - checks if char is alphabet or not
+ *@c: Characters are i ASCII code
  * Return: 1 if c is a letter and 0 if otherwise
 **/
 int _isalpha(int c)
