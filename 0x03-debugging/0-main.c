d5792583c6a95be18542d948218ba784d2b5440c
Raheem Salifu Kasim <r.kasim@aol.com>
2022-06-08 20:11:00 +0000

    Debugging tasks

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - function to test if printed integer is positive or negative
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
