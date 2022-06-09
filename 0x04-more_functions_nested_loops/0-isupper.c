e0e67fe7c12e0649282b91b6468187ff22669305
Raheem Salifu Kasim <r.kasim@aol.com>
2022-06-09 18:38:26 +0000

    More functions Tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _isupper - checks if input is an uppercase letter in ASCII
@@ -7,5 +7,12 @@
  */
 int _isupper(int c)
 {
-	return (c >= 65 && c <= 90);
+  if (c >= 'A' && c <= 'Z')
+    {
+      return (1);
+    }
+  else
+    {
+      return (0);
+    }
 }
