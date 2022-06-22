8126e4ba5859d1512969ef0268cbafe1b4946b5d
Raheem Salifu Kasim <r.kasim@aol.com>
2022-06-22 23:27:24 +0000

    0x08. C - Recursion task

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,20 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *  _pow_recursion - calculates value of x raised to the power of y
- * @x: given number
- * @y: exponent
- * Return: value of x raised to the power of y
- **/
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: number to be squared
+ * @y: power of x to be squared
+ * Return: result of x**y
+ */
 int _pow_recursion(int x, int y)
 {
-  if (y < 0)
-    {
-      return (-1);
-    }
-  if (y == 0)
-    {
-      return (1);
-    }
-  return (x * _pow_recursion(x, y - 1));
+		if (y < 0)
+			return (-1);
+		else if (y == 0)
+			return (1);
+		return (_pow_recursion(x, y - 1) * x);
 }
