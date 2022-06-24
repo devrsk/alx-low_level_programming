9b959b622c888692cbafe762c02f219f93424fb9
Raheem Salifu Kasim <r.kasim@aol.com>
2022-06-24 22:58:37 +0000

    Update

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs - compute absolute value of an integer
+ *@n: integer input
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
