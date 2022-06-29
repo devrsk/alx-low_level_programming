2116a2978a43a524450b1771248ec1640065795e
Raheem Salifu Kasim <rsalifu@yahoo.com>
2022-06-29 09:59:27 -0700

    Update Task

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,17 @@
 #include "main.h"
 
 /**
- * _isupper - checks if input is an uppercase letter in ASCII
- * @c: integer to check
- * Return: 1 if true, 0 if false
+ * _isupper - checks if a character is upper
+ * #@c: carrier integer variable
+ * Return: 1 if true, 9 if false
  */
+
 int _isupper(int c)
 {
-	if (c > 64 && c < 91)
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 
 	return (0);
 }
