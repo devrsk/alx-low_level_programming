ed4f746f4b1e40db0509b27b9f78b22b0acf964e
Raheem Salifu Kasim <r.kasim@aol.com>
2022-07-18 23:29:20 +0000

    Task Update

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,36 +1,34 @@
-include "lists.h"
+#include "lists.h"
+
 /**
- * find_listint_loop - found a loop and the begining of loop
- * @head: pointer to head in function
- * Return: pointer to the begining of loop
+ * find_listint_loop - a function that finds the loop in a linked list
+ *
+ * @head: the head of the singly linked list
+ *
+ * Return: returns the address of the node where the loop starts
+ * and returns NULL if there is no loop
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *tortoise;
-	listint_t *hare;
-
-	if (head == NULL || head->next == NULL)/*If the list is null or just 1 node*/
-		return (NULL);
-
-	tortoise = hare = head;/*Start at the same point*/
-
-	while (tortoise && hare && hare->next)
-	{
-		tortoise = tortoise->next;/*1 step hop*/
-		hare = hare->next->next;/*2 steps hop*/
-		if (tortoise == hare)
-			break;
-	}
-
-	if (tortoise != hare) /* if the while ended until a NULL*/
-		return (NULL);
-
-	tortoise = head; /*re-start tortoise*/
-
-	while (tortoise != hare)
-	{
-		tortoise = tortoise->next;
-		hare = hare->next;
-	}
-	return (hare);
+listint_t *slow, *fast;
+if (head == NULL)
+return (NULL);
+slow = head, fast = head;
+while (slow && fast && fast->next)
+{
+slow = slow->next, fast = fast->next->next;
+if (slow == fast)
+break;
+}
+if (slow && fast && fast->next)
+{
+slow = head;
+while (1)
+{
+slow = slow->next, fast = fast->next;
+if (fast->next == slow->next)
+return (slow->next);
+}
+}
+return (NULL);
 }
