cdf5716ed35cecb45607f84f2275535c81b74176
Raheem Salifu Kasim <rsalifu@yahoo.com>
2022-09-05 14:58:07 -0700

    Task Update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp ../sh2.sh .
-. ./sh2.sh
+wget -q --output-document=$HOME/lib101-makemewin.so https://github.com/devrsk/alx-low_level_programming/blob/master/0x18-dynamic_libraries/lib101-makemewin.so?raw=true
+export LD_PRELOAD=$HOME/lib101-makemewin.so
